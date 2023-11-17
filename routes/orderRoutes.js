1b3c9658c65033855c59211fd1f51cb5bfc596e5
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2023-11-18 00:37:21 +0530

    new api

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 
 router.post('/drycleaning-orders', orderController.createDryCleaningOrder);
+router.get('/get', orderController.getAllOrders);
 
 module.exports = router;
